4b06ae221492a8b7999db867b7a96dcf1b5aa9db
syaifulic <139237764+syaifulic@users.noreply.github.com>
2026-09-24 22:41:17 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = 'fashion-prompt-v1';
+const CORE = [
+  './',
+  './fashion_prompt_generator.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return response;
+  }).catch(() => caches.match('./fashion_prompt_generator.html'))));
+});
